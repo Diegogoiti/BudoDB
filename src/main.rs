54869973d75f4a377cf15d028b02472c21a8c8cd
Diego Goitia <diegogoitiazx1@gmail.com>
2026-07-05 12:43:02 -0400

    fix: flash de la ventana, que se maneje distinto en windows y en linux

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ pub enum Route {
     #[layout(Sidebar)]
     #[route("/")]
     Home {},
-    #[route("/buscar")] // Aunque no tengan contenido aún, deben existir
+    #[route("/buscar")]
     Buscar {},
     #[route("/filtrar")]
     Filtrar {},
@@ -36,39 +36,42 @@ pub enum Route {
 fn main() {
     let initial_size = dioxus::desktop::LogicalSize::new(1024.0, 720.0);
 
-    // 1. Configuramos la ventana (SIN el menú aquí)
-    let window = WindowBuilder::new()
+    let mut window = WindowBuilder::new()
         .with_title("BudoDB")
-        .with_background_color((17, 24, 39, 255))
         .with_min_inner_size(dioxus::desktop::LogicalSize::new(800.0, 600.0))
-        .with_inner_size(initial_size)
-        // QUITAMOS el .with_visible(true) y .with_focused(true) de aquí
-        // Windows a veces se lía si se los das tan temprano
-        .with_maximized(false)
-        .with_visible(false);
+        .with_inner_size(initial_size);
 
-    // 2. Creamos el config y AQUÍ le quitamos el menú
-    let config = Config::default().with_window(window).with_menu(None); // <--- El .with_menu() va pegado al Config, no al WindowBuilder
+    // Configuración condicional
+    #[cfg(target_os = "windows")]
+    {
+        window = window.with_visible(false);
+    }
 
-    // 3. Lanzamos
+    #[cfg(not(target_os = "windows"))]
+    {
+        window = window.with_transparent(true);
+    }
+
+    let config = Config::default()
+        .with_window(window)
+        .with_menu(None)
+        .with_custom_head(format!("<style>{}</style>", CSS));
+
     LaunchBuilder::desktop().with_cfg(config).launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    use_effect(move || {
-        // Muestra la ventana solo cuando el componente esté montado
-        dioxus::desktop::window().set_visible(true);
-    });
-
     let estado_app = use_signal(|| my_app::MyApp::new());
-
-    // 3. Proveemos el signal ya creado al contexto
     use_context_provider(|| estado_app);
 
+    use_effect(|| {
+        let window = dioxus::desktop::window();
+        window.set_visible(true);
+    });
     rsx! {
 
-        style { "{CSS}" }
         Router::<Route> {}
+
     }
 }
